0b0de5a8027b3644168866755524b8e004a9f9ff
Abdou Khoudoss <khoudossmbacke18@gmail.com>
2025-01-08 17:32:16 +0000

    xawma sax

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
       setIsLoading(true);
 
       try {
-        const response = await fetch('https://auth-backend-3-mw39.onrender.com/api/login', {
+        const response = await fetch('http://localhost:5000/api/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email, password }),
--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -10,7 +10,7 @@ const ResetPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://auth-backend-3-mw39.onrender.com/api/reset-password', {
+      const response = await fetch('http://localhost:5000/api/reset-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ resetToken, newPassword }),
